c50b336c33de87e9f314f2d72f93dd1b94665131
GustavoDng <gustavo.deininger@gmail.com>
2017-08-21 09:46:25 -0300

    git push

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
-    url(r'^teste/', views.teste),
-    url(r'^menu/', views.menu),
-    url(r'^credts/', views.credts),
-    url(r'^projects/', views.projects),
-]
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    ]
